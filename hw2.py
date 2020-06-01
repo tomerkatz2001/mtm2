72607939f676afff2928588c561031b932565ce0
Magnido9 <40192962+Magnido9@users.noreply.github.com>
2020-06-01 12:25:06 +0300

    Add files via upload

--- hw2.py
+++ hw2.py
@@ -31,7 +31,7 @@ def readParseData(file_name):
                     database.append(item)
             cheater=False
             line = file.readline()
-        file.close()
+
         return database
 
 
@@ -63,7 +63,7 @@ def sumAllUp(type_set, type_dict):
     return sum_list
 
 
-def calcCompetitionResults(competitors_in_competitions):
+def calcCompetitionsResults(competitors_in_competitions):
     timed_list = []
     untimed_list = []
     knockout_list = []
@@ -136,6 +136,94 @@ def calcCompetitionResults(competitors_in_competitions):
     return returned_list
 
 
-print(readParseData("2020.txt"))
-print(calcCompetitionResults(readParseData("2020.txt")))
+def printCompetitor(competitor):
+    '''
+    Given the data of a competitor, the function prints it in a specific format.
+    Arguments:
+        competitor: {'competition name': competition_name, 'competition type': competition_type,
+                        'competitor id': competitor_id, 'competitor country': competitor_country,
+                        'result': result}
+    '''
+    competition_name = competitor['competition name']
+    competition_type = competitor['competition type']
+    competitor_id = competitor['competitor id']
+    competitor_country = competitor['competitor country']
+    result = competitor['result']
+
+    assert (isinstance(result, int))  # Updated. Safety check for the type of result
+
+    print(f'Competitor {competitor_id} from {competitor_country} participated in {competition_name} ({competition_type}) and scored {result}')
+
+
+def printCompetitionResults(competition_name, winning_gold_country, winning_silver_country, winning_bronze_country):
+    '''
+    Given a competition name and its champs countries, the function prints the winning countries
+        in that competition in a specific format.
+    Arguments:
+        competition_name: the competition name
+        winning_gold_country, winning_silver_country, winning_bronze_country: the champs countries
+    '''
+    undef_country = 'undef_country'
+    countries = [country for country in [winning_gold_country, winning_silver_country, winning_bronze_country] if
+                 country != undef_country]
+    print(f'The winning competitors in {competition_name} are from: {countries}')
+
+
+def key_sort_competitor(competitor):
+    '''
+    A helper function that creates a special key for sorting competitors.
+    Arguments:
+        competitor: a dictionary contains the data of a competitor in the following format:
+                    {'competition name': competition_name, 'competition type': competition_type,
+                        'competitor id': competitor_id, 'competitor country': competitor_country,
+                        'result': result}
+    '''
+    competition_name = competitor['competition name']
+    result = competitor['result']
+    return (competition_name, result)
+
+
+
+
+
+
+def partA(file_name='input.txt', allow_prints=True):
+    # read and parse the input file
+    competitors_in_competitions = readParseData(file_name)
+    if allow_prints:
+        # competitors_in_competitions are sorted by competition_name (string) and then by result (int)
+        for competitor in sorted(competitors_in_competitions, key=key_sort_competitor):
+            printCompetitor(competitor)
+
+    # calculate competition results
+    competitions_results = calcCompetitionsResults(competitors_in_competitions)
+    if allow_prints:
+        for competition_result_single in sorted(competitions_results):
+            printCompetitionResults(*competition_result_single)
+
+    return competitions_results
+
+import Olympics
+def partB(file_name='input.txt'):
+    competitions_results = partA(file_name, allow_prints=False)
+    olympic=Olympics.OlympicsCreate()
+    for item in competitions_results:
+        Olympics.OlympicsUpdateCompetitionResults(olympic,str(item[1]),str(item[2]),str(item[3]))
+    Olympics.OlympicsWinningCountry(olympic)
+    Olympics.OlympicsDestroy(olympic)
+    # TODO Part B
+
+
+if __name__ == "__main__":
+    '''
+    The main part of the script.
+    __main__ is the name of the scope in which top-level code executes.
+
+    To run only a single part, comment the line below which correspondes to the part you don't want to run.
+    '''
+    file_name = 'input.txt'
+
+    partA("2020.txt")
+    partB("2020.txt")
+
 
